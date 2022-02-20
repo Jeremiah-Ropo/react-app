1c303016522f0faff3146732500e218a8e3deb5e
Patrick Jeremiah <patrickjerry59@yahoo.com>
2022-02-20 16:53:54 +0100

    in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,55 @@
 import React, {useState} from 'react';
+import TodoItems from './todoItem';
+import Header from './Header';
 
 function App(){
     const [inputText, setInput] = useState("");
     const [items, setItems] = useState([]);
 
+    function addItems(){
+        setItems(prevItems => {
+            return [...prevItems, inputText ]
+        });
+        setInput("");
+    }
+
+
+    function handleChange(event){
+        const newValue = event.target.value;
+        setInput(newValue);
+    }
+
+    function deleteitems(id){
+        setItems(prevItems =>{
+            return prevItems.filter((item, index)=>{
+                return index !== id 
+            })
+        })
+    }
 
     return (
         <div className="container">
             <div className="heading">
-                <h1>To-Do List</h1>
+              <Header />
             </div>
             <div className="form">
-                <input type="text"/>
-                <button >
+                <input type="text" onChange={handleChange} value={inputText}/>
+                <button onClick={addItems}>
                     <span>Add</span>
                 </button>
             </div>
             <div>
-                <ul>
-             
-                </ul>
+            <ul>
+                {items.map((todoItems, index) => 
+                <TodoItems
+                    key={index}
+                    id={index}
+                    onChecked ={deleteitems}
+                    item={todoItems}
+                />)}
+             </ul>
             </div>
+
         </div>
 
     )
---
+++ src/components/todoItem.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+function TodoItems(props){
+
+    return(
+        <div onClick={
+            ()=>{
+                props.onChecked(props.id)
+            }
+        }>
+            <li>{props.item}</li>
+        </div> 
+
+    )
+};
+
+export default TodoItems;
